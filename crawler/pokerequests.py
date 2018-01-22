b912b1beef82b34b62ff2e36e154966e2c31dc8b
Qu DoNG <qudong452@gmail.com>
2018-01-22 18:19:53 +0800

    more pep8; added representation of pokemon obj cls; added the filter in the main
    Signed-off-by: Qu DoNG <qudong452@gmail.com>

--- crawler/pokerequests.py
+++ crawler/pokerequests.py
@@ -17,37 +17,41 @@ from datetime import timedelta
 from geopy.distance import vincenty
 
 try:
-    from ..wheel.color import * 
+    from ..wheel.color import *
 except ValueError:
     from wheel.color import *
 except SystemError:
     import sys
-    from pathlib import Path # new in 3.4
+    from pathlib import Path  # new in 3.4
+
     _pp_dir = str(Path(__file__).parents[1])
     sys.path.insert(0, _pp_dir)
     from wheel.color import *
 
 DEBUG = False
 
-
 FILE = os.path.dirname(__file__)
+
 
 def PokeCrawlerError(Exception):
     pass
 
 
-def is_nearby(poke_lat_lng, current_lat_lng=(1.457501, 133.812063), radius=500):
+def is_nearby(poke_lat_lng, current_lat_lng=(1.457501, 133.812063),
+              radius=500):
     return vincenty(current_lat_lng, poke_lat_lng).meters <= radius
 
 
-def get_favor_poke_ids(pokejson= "favourite_poke.json"):
+def get_favor_poke_ids(pokejson="favourite_poke.json"):
     """https://github.com/rkern/line_profiler/issues/37
     """
-    with open(os.path.join(FILE, 'favourite_poke.json'), encoding='utf-8') as data_file:
+    with open(os.path.join(FILE, 'favourite_poke.json'),
+              encoding='utf-8') as data_file:
         pokelist = json.load(data_file)
     poke_ids = [x["i"] for x in pokelist]
     return poke_ids, pokelist
-    
+
+
 _, _fv_pklist = get_favor_poke_ids()
 
 _myfavourite_pokes_dict = {}
@@ -55,7 +59,8 @@ for fv_pk in _fv_pklist:
     if fv_pk in list(_myfavourite_pokes_dict.keys()):
         raise PokeCrawlerError("My Json is wrong!")
     _myfavourite_pokes_dict[int(fv_pk['i'])] = fv_pk['n']
-    
+
+
 def getDataFromSGPokeMap():
     """
     todo-qd: fixed the InsecureRequestWarning issue
@@ -65,35 +70,40 @@ def getDataFromSGPokeMap():
     list_of_pokemon_ids = ','.join(get_favor_poke_ids()[0])
     current_epoch_time = 0
     with requests.Session() as s:
-        s.headers = { # request headers setting
-            "authority": "sgpokemap.com",
-            "method": "GET",
-            "path": "/query2.php?since=" + str(current_epoch_time) + "&mons=" + list_of_pokemon_ids,
-            "scheme": "https",
-            "accept": "*/*",
-            "accept-encoding": "gzip, deflate, sdch, br",
-            "accept-language": "en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,ms;q=0.2",
-            "cache-control": "no-cache",
-#            "cookie": "__cfduid=d4d51b55890b0ecaed8a096ca9e8bd8b01511605718; _ga=GA1.2.2119617142.1511605711; _gat=GA1.2.65321968.1515334414",
-            "pragma": "no-cache",
-            "referer": "https://sgpokemap.com/",
-            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36",
-            "x-requested-with": "XMLHttpRequest"
-        }
-    
-        r = s.get("https://sgpokemap.com/query2.php?since=" + str(current_epoch_time) + "&mons=" + list_of_pokemon_ids,
+        s.headers = {  # request headers setting
+                       "authority": "sgpokemap.com",
+                       "method": "GET",
+                       "path": "/query2.php?since=" + str(
+                           current_epoch_time) + "&mons=" + list_of_pokemon_ids,
+                       "scheme": "https",
+                       "accept": "*/*",
+                       "accept-encoding": "gzip, deflate, sdch, br",
+                       "accept-language": "en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,ms;q=0.2",
+                       "cache-control": "no-cache",
+                       #            "cookie": "__cfduid=d4d51b55890b0ecaed8a096ca9e8bd8b01511605718; _ga=GA1.2.2119617142.1511605711; _gat=GA1.2.65321968.1515334414",
+                       "pragma": "no-cache",
+                       "referer": "https://sgpokemap.com/",
+                       "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36",
+                       "x-requested-with": "XMLHttpRequest"
+                       }
+
+        r = s.get("https://sgpokemap.com/query2.php?since=" + str(
+            current_epoch_time) + "&mons=" + list_of_pokemon_ids,
                   verify=False)
 
         if r.status_code != 200:
-            PokeCrawlerError("Crawling Failed with status code: {}".format(r.status_code)) 
-    response = r.json() # decoding json to dict
+            PokeCrawlerError(
+                "Crawling Failed with status code: {}".format(r.status_code))
+    response = r.json()  # decoding json to dict
     if DEBUG:
         colorprint(response.keys(), color="blue")
-        colorprint("In Total, {} Pokemons were found".format(len(response['pokemons'])), color="blue")
-#        colorprint(response['pokemons'][0], color="magenta")
+        colorprint("In Total, {} Pokemons were found".format(
+            len(response['pokemons'])), color="blue")
+    # colorprint(response['pokemons'][0], color="magenta")
     poke_all_sg = response['pokemons']
     return poke_all_sg
-    
+
+
 def get_favor_poke_nearby(pokescanned, **kwargs):
     nearby_pm = []
     for pm in pokescanned:
@@ -102,11 +112,29 @@ def get_favor_poke_nearby(pokescanned, **kwargs):
             nearby_pm.append(pm)
     return nearby_pm
 
-class Dict2Cls:
+
+class PokeDict2Cls:
     def __init__(self, **entries):
+        self.pkdict = entries
         self.__dict__.update(entries)
-        
-        
+
+    def __repr__(self):
+        if self.cp:
+            pkstr = "{} - (IV:{}%) - (CP: {}) - (Level: {})".format(self.name,
+                                                                    self.iv,
+                                                                    self.cp,
+                                                                    self.level)
+            pkstr += "\nWeather Boost: {}".format(
+                True if self.weather > 0 else False)
+            pkstr += "\nL30+ Stats: ({}) {}".format(self.stats, self.iv)
+            pkstr += "\nL30+ CP: {} (Level: {})".format(self.cp, self.level)
+        else:
+            pkstr = "{}".format(self.name)
+        pkstr += "\nUntil: {} ({} left)".format(self.despawn, self.time_left)
+        pkstr += "\nLocation: ({}, {})".format(*self.location)
+        return pkstr
+
+
 def poke_detail_deserialization(**poke_info_dict):
     """
     JSON is a format that encodes objects in a string. Serialization means 
@@ -121,12 +149,13 @@ def poke_detail_deserialization(**poke_info_dict):
     'attack': '15', 'move2': '14', 'shiny': '0', 'lat': '1.41604088', 
     'despawn': '1516605285', 'weather': '0'}
     """
-    for k,v in poke_info_dict.items():
-        if k =="despawn":
-            poke_info_dict[k] = time.strftime("%I:%M:%S%p", time.localtime(float(v)))
+    for k, v in poke_info_dict.items():
+        if k == "despawn":
+            poke_info_dict[k] = time.strftime("%I:%M:%S%p",
+                                              time.localtime(float(v)))
             t = time.time()
             seconds_left = int(float(v) - t)
-            time_left = "{}:{}".format(seconds_left//60, seconds_left%60)
+            time_left = "{}:{}".format(seconds_left // 60, seconds_left % 60)
         elif k in ('lat', 'lng'):
             pass
         else:
@@ -135,15 +164,17 @@ def poke_detail_deserialization(**poke_info_dict):
             else:
                 poke_info_dict[k] = None
     poke_info_dict['time_left'] = time_left
-    
+
     poke_info_dict['stats'] = [None, None, None]
-    poke_info_dict['location'] = [None, None] # [lat, lng]
-    for k,v in poke_info_dict.items():
+    poke_info_dict['location'] = [None, None]  # [lat, lng]
+    for k, v in poke_info_dict.items():
         if k == "pokemon_id":
             try:
                 pk_name = _myfavourite_pokes_dict[v]
             except KeyError:
-                raise PokeCrawlerError("Scanned Pokemon ID: {}is NOT in my favrouite list".format(v))
+                raise PokeCrawlerError(
+                    "Scanned Pokemon ID: {}is NOT in my favrouite list".format(
+                        v))
         elif k == "attack":
             poke_info_dict['stats'][0] = v
         elif k == "defence":
@@ -159,41 +190,41 @@ def poke_detail_deserialization(**poke_info_dict):
         poke_info_dict["iv"] = None
         poke_info_dict["stats"] = None
     else:
-        poke_info_dict["iv"] = round(sum(poke_info_dict['stats']) / 45.0, 2)
+        poke_info_dict["iv"] = round(sum(poke_info_dict['stats']) / 45.0 * 100,
+                                     1)
 
-    poke_obj = Dict2Cls(**poke_info_dict)
-    
+    poke_obj = PokeDict2Cls(**poke_info_dict)
+
     return poke_obj
-    
+
+
 def get_nearby_pkm_obj_list(**kwargs):
     poke_all_sg = getDataFromSGPokeMap()
     nearby_pm = get_favor_poke_nearby(poke_all_sg, **kwargs)
     nby_pm_obj_lst = [poke_detail_deserialization(**x) for x in nearby_pm]
     return nby_pm_obj_lst
-        
+
+
 def main(**kwargs):
     for i in kwargs:
-        if i=="radius":
+        if i == "radius":
             radius = kwargs[i]
-        if i=="loc":
+        if i == "loc":
             loc = kwargs[i]
-    pkms = get_nearby_pkm_obj_list(current_lat_lng=loc, radius = radius)
-    colorprint("\nBased on the current location {}, within radius of {}m, {} Pokemons are found".format(loc, radius, len(pkms)), color="blue")
+    pkms = get_nearby_pkm_obj_list(current_lat_lng=loc, radius=radius)
+    colorprint(
+        "\nBased on the current location {}, within radius of {}m, {} My Favourite Pokemons are found".format(
+            loc, radius, len(pkms)), color="blue")
     for pkm in pkms:
         if pkm.cp:
-            colorprint("===================================================", color="red")
-            colorprint("{} - (IV:{}) - (CP: {}) - (Level: {})".format(pkm.name, pkm.iv, pkm.cp, pkm.level), color="red")
-            colorprint("Until: {} ({} left)".format(pkm.despawn, pkm.time_left), color="red")
-            colorprint("Weather Boost: {}".format(True if pkm.weather>0 else False), color="red")
-            colorprint("L30+ Stats: ({}) {}".format(pkm.stats, pkm.iv), color="red")
-            colorprint("L20+ CP: {} (Level: {})".format(pkm.cp, pkm.level), color="red")
-            colorprint("Location: {}".format(pkm.location), color="red")
+            colorprint("===================================================",
+                       color="red")
+            colorprint(pkm, color="red")
         else:
-            colorprint("===================================================", color="magenta")
-            colorprint("{}".format(pkm.name, pkm.iv, pkm.cp, pkm.level), color="magenta")
-            colorprint("Until: {} ({} left)".format(pkm.despawn, pkm.time_left), color="magenta")
-            colorprint("Location: {}".format(pkm.location), color="magenta")
-    
+            colorprint("===================================================",
+                       color="magenta")
+            colorprint(pkm, color="magenta")
+
+
 if __name__ == "__main__":
     pass
-    
--- run_crawler.py
+++ run_crawler.py
@@ -12,4 +12,13 @@ import crawler.pokerequests as pokemaxcrawler
 loch = (1.439399, 103.801980)
 loco = (1.447501, 103.812063)
 
-pokemaxcrawler.main(loc = loco, radius=500)
+pokemaxcrawler.main(loc=loco, radius=500)
+
+pkms = pokemaxcrawler.get_nearby_pkm_obj_list(current_lat_lng=loco,
+                                              radius=5000)
+
+for pkm in pkms:
+    if pkm.level and pkm.iv > 80 and pkm.level >= 25:
+        pokemaxcrawler.colorprint(
+            "===================================================", color="red")
+        pokemaxcrawler.colorprint(pkm, color="red")
